1b1b92851a2de1b8a3f17ea5958272d1845b03e2
wadezha <wade.zha@icecobotics.com>
2024-01-05 14:45:57 +0800

    fix clients

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ module.exports = app => {
   if (app.config.mqtt.app) mqtt(app);
 
   app.ready(() => {
-    app.mqtt.init();
-    app.mqtt.config.topics.forEach(async (t) =>  {
-      await app.mqtt.channel.subscribe(t.topic, t.options);
-    });  
+    if (app.config.mqtt.client) {
+      app.mqtt.init();
+      app.mqtt.config.topics.forEach(async (t) =>  {
+        await app.mqtt.channel.subscribe(t.topic, t.options);
+      }); 
+    }
+    if (app.config.mqtt.clients) {
+      for (const k of Object.keys(app.config.mqtt.clients)) {
+        app.mqtt.get(k).init();
+        app.mqtt.get(k).config.topics.forEach(async (t) =>  {
+          await app.mqtt.get(k).channel.subscribe(t.topic, t.options);
+        }); 
+      }
+    } 
   });
 };
--- config/config.default.js
+++ config/config.default.js
@@ -3,30 +3,84 @@
 exports.mqtt = {
   app: true,
   agent: false,
-  client: {
-    host: 'mqtt://127.0.0.1',
-    port: 1883,
-    username: 'username',
-    password: 'password',
-    clientId: 'client_id',
-    options: {
-      keeplive: 60,
-      reconnectPeriod: 1000,
-      connectTimeout: 30 * 1000,
-      protocolId: 'MQTT',
-      protocolVersion: 4,
-      clean: true,
-      rejectUnauthorized: false,
-      will: {
-        topic: 'client_id',
-        payload: 'offline',
-        qos: 2,
-        retain: true
-      },
-    },
-    onlinePayload: '',
-    topics: [],
-    inMiddleware: [],
-    outMiddleware: [],
-  }
+  // client: {
+  //   host: 'mqtt://127.0.0.1',
+  //   port: 1883,
+  //   username: 'username',
+  //   password: 'password',
+  //   clientId: 'client_id',
+  //   options: {
+  //     keeplive: 60,
+  //     reconnectPeriod: 1000,
+  //     connectTimeout: 30 * 1000,
+  //     protocolId: 'MQTT',
+  //     protocolVersion: 4,
+  //     clean: true,
+  //     rejectUnauthorized: false,
+  //     will: {
+  //       topic: 'client_id',
+  //       payload: 'offline',
+  //       qos: 2,
+  //       retain: true
+  //     },
+  //   },
+  //   onlinePayload: '',
+  //   topics: [],
+  //   inMiddleware: [],
+  //   outMiddleware: [],
+  // }
+  // clients: {
+  //   mqtt1: {
+  //     host: 'mqtt://127.0.0.1',
+  //     port: 1883,
+  //     username: 'username',
+  //     password: 'password',
+  //     clientId: 'client_id',
+  //     options: {
+  //       keeplive: 60,
+  //       reconnectPeriod: 1000,
+  //       connectTimeout: 30 * 1000,
+  //       protocolId: 'MQTT',
+  //       protocolVersion: 4,
+  //       clean: true,
+  //       rejectUnauthorized: false,
+  //       will: {
+  //         topic: 'client_id',
+  //         payload: 'offline',
+  //         qos: 2,
+  //         retain: true
+  //       },
+  //     },
+  //     onlinePayload: '',
+  //     topics: [],
+  //     inMiddleware: [],
+  //     outMiddleware: [],
+  //   },
+  //   mqtt2: {
+  //     host: 'mqtt://127.0.0.1',
+  //     port: 1883,
+  //     username: 'username',
+  //     password: 'password',
+  //     clientId: 'client_id',
+  //     options: {
+  //       keeplive: 60,
+  //       reconnectPeriod: 1000,
+  //       connectTimeout: 30 * 1000,
+  //       protocolId: 'MQTT',
+  //       protocolVersion: 4,
+  //       clean: true,
+  //       rejectUnauthorized: false,
+  //       will: {
+  //         topic: 'client_id',
+  //         payload: 'offline',
+  //         qos: 2,
+  //         retain: true
+  //       },
+  //     },
+  //     onlinePayload: '',
+  //     topics: [],
+  //     inMiddleware: [],
+  //     outMiddleware: [],
+  //   },
+  // }
 };
